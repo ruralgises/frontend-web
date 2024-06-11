57168893e90a34856fd8a648cdbdc20c47757b5f
Heflain Mendes <heflain@outlook.com.br>
2024-06-10 21:13:41 -0300

    feat: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MatSidenavModule],
+  template: `
+    <mat-sidenav-container>
+      <router-outlet></router-outlet>
+    </mat-sidenav-container>
+  `,
 })
 export class AppComponent {
   title = 'frontend-web';
